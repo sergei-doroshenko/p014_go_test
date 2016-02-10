b132264b15354c72e8f29a69bb8d0c370993037c
sergei-doroshenko <6823298@gmail.com>
2016-02-11 00:19:51 +0300

    created posts and handlers

--- main.go
+++ main.go
@@ -2,8 +2,59 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/sergei-doroshenko/p014_go_test/models"
+	"github.com/sergei-doroshenko/p014_go_test/utils"
 )
 
+var posts map[string]*models.Post
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "<h1>Hello, Sergei!!!!</h1>")
+	t, err := template.ParseFiles("views/index.html", "views/header.html", "views/footer.html") //
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Println(posts) // write all posts
+
+	t.ExecuteTemplate(w, "index", posts)
+}
+
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("views/write.html", "views/header.html", "views/footer.html") //
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	// id := r.FormValue("id")
+	id := utils.GeneratId()
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	post := models.NewPost(id, title, content)
+	posts[post.Id] = post
+
+	http.Redirect(w, r, "/", 302)
+}
+
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Server started on port :3000")
+
+	posts = make(map[string]*models.Post, 0)
+
+	// we need to cut 'public' and left /css/app.css for example
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
+	http.HandleFunc("/SavePost", savePostHandler)
+
+	http.ListenAndServe(":3000", nil)
 }
